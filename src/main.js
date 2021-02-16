4fdf21c90577d42949d89c3ca5dc392f28bf1914
Carlos Vera A <carveraa13@gmail.com>
2021-02-16 04:12:27 +0000

    Update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRamdomSuit());
+  document.querySelector(".card").innerHTML = generateRamdomNumber();
+};
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+let generateRamdomNumber = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRamdomSuit = () => {
+  var suit = ["diamond", "spades", "heart", "club"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  return suit[indexSuit];
 };
